ceac8478e977a16f92e9a4abb774e399287f0039
Josh Smith <purejosh12@gmail.com>
2020-03-12 20:31:59 -0600

    js

---
+++ JS/helloworld.js
@@ -0,0 +1,24 @@
+document.write("Hello world");
+
+var A = "This is a string";
+var A = A.fontcolor("red");
+document.write(A);
+
+var Z = "hello";
+var A = A.fontcolor("red");
+
+window.alert(Z);
+
+document.write("Hello \"Jim\"");
+
+
+var j ="\"I love tacobell so much\"\, - \"Josh\"";
+var j = j.fontcolor("red");
+document.write(j)
+
+var D = "Dog", C = "Cat";
+var D = D.fontcolor("red");
+var C = C.fontcolor("red");
+document.write(D)
+
+document.write(3 + 3)
