64bef8792bfd82c1342294f347ccd711380b522c
Josh Smith <purejosh12@gmail.com>
2020-03-16 09:06:52 -0600

    js6

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -1,13 +1,52 @@
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color= Color;
+var X = 5;
+
+function math1Function() {
+    document.getElementById("Math1").innerHTML = (X + 10);
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML=
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " Manufactured in " + Erik.Vehicle_Year;
+
+function tmath2Function() {
+    var Y = 15;
+    document.getElementById("Math2").innerHTML = (Y + 10);
+}
+
+function get_Date() {
+    if (new Date().getHours() < 18)  {
+    document.getElementById("hello").innerHTML = "How are you this evening?";
+    }
+}
+
+console.log()
+
+function math3Function() {
+    var X = 10
+    var Y = 15
+    if (Y > X) {
+    document.getElementById("Math3").innerHTML = "15 is greater than 10"
+    }
+}
+
+function Voter_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Words = "You are old enough to vote!";
+    }
+    else {
+        Words = "You are not old enough to vote!";
+    }
+    document.getElementById("Vote").innerHTML = Words;
+}
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Replay = "It s morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Replay = "It is the afternoon.";
+    }
+    else {
+        Replay = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Replay;
 }
