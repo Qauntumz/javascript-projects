15bad208f1a7137d3483c8656e96fbc06162a313
Josh Smith <purejosh12@gmail.com>
2020-03-18 10:44:29 -0600

    js11

--- JS/Basic_Javascript_10.js
+++ JS/Basic_Javascript_10.js
@@ -32,3 +32,51 @@ function cat_actions() {
     Cat_Action[3] = "purring";
     document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Action[2] + ".";
 }
+
+function constant_function() {
+    const Musical_instrument = {type: "guitar", brand:"Fender", color:"black"};
+    Musical_instrument.color = "blue";
+    Musical_instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_instrument.type + " was " + Musical_instrument.price + " and the color was " + Musical_instrument.color;
+}
+
+var X = 100 
+document.write(X);
+{
+    let X = 50;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+function myfunction() {
+    return Math.PI;
+}
+document.getElementById("demo").innerHTML = myfunction();
+
+let car = {
+    make: "Ford",
+    model: "Mustang",
+    year: "2020",
+    color: "Red",
+    description : function() {
+        return "the car is a " + this.year + " " + this.color + " " + this.make + " " + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+
+var ZZ = "";
+var i;
+for (i = 0; i < 10; i++) {
+  if (i === 3) { break; }
+  ZZ += "The number is " + i + "<br>";
+}
+document.getElementById("part1").innerHTML = ZZ;
+
+var HH = "";
+var k;
+for (k = 0; k < 10; k++) {
+  if (k === 3) { continue; }
+  HH += "The number is " + k + "<br>";
+}
+document.getElementById("part2").innerHTML = HH;
